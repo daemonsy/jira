0addddc776a16568437402f7ce20f532826fcee9
Damon Aw <daemonsy@gmail.com>
2018-04-20 17:49:32 -0400

    Don't recognize a pure string of digits as a ticket

--- src/utilities/ticket-match.js
+++ src/utilities/ticket-match.js
@@ -1,8 +1,13 @@
-const properTicketMatch = /\w+-\d+/;
-const dirtyTicketMatch = /^\w+\d+$/;
+const properTicketMatch = /\w+-\d+/; // LPJ-1234
+const dirtyTicketMatch = /^\w+\d+$/; // e.g. LPJ1234
+const digitsOnlyMismatch = /^\d+$/; // e.g. 1065
 
 export default text => {
   switch(true) {
+    case digitsOnlyMismatch.test(text): {
+      return null;
+    }
+
     case properTicketMatch.test(text): {
       return text;
     }
