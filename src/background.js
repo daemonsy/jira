d19871811a5d140ab6ac6949d2f2274a1bba89e5
Damon Aw <daemonsy@gmail.com>
2018-10-05 13:18:03 -0400

    Jump to project if key is parsed to be a project key

--- src/background.js
+++ src/background.js
@@ -5,71 +5,110 @@ import {
   buildHelper,
   apiSearchIssuesPath,
   apiIssuePath,
-  pageSearchIssuesPath
+  apiProjectPath,
+  apiProjectsPath,
+  pageSearchIssuesPath,
 } from './jira/paths';
 
-const escapeEntities = (text) => {
+const escapeEntities = text => {
   // Dependent on Browser API
   const converter = document.createElement('span');
   converter.textContent = text;
-  return converter.innerHTML
-}
+  return converter.innerHTML;
+};
 
 const messages = {
   defaultPrompt: 'Type in a ticket number or free text to search for issues',
-  noSubdomain: 'Jira subdomain needs to be entered. Click here or hit enter to open the Settings page',
+  noSubdomain:
+    'Jira subdomain needs to be entered. Click here or hit enter to open the Settings page',
   typeMore: 'Nothing yet. Keep typing :)'
 };
 
 const makeJiraApiCall = url =>
-  fetch(url, { credentials: 'same-origin' })
-    .then(response => response.json());
+  fetch(url, { credentials: 'same-origin' }).then(response => response.json());
 
 const showDefaultSuggestion = description =>
-  chrome.omnibox.setDefaultSuggestion({ description: escapeEntities(description) });
+  chrome.omnibox.setDefaultSuggestion({
+    description: escapeEntities(description)
+  });
 
 const displayRelevantSuggestions = (input, suggest, subdomain) => {
   const apiSearchIssuesURL = buildHelper(subdomain, apiSearchIssuesPath);
+  const apiProjectsURL = buildHelper(subdomain, apiProjectsPath);
 
   showDefaultSuggestion(`Search for ${input}...`);
-  makeJiraApiCall(apiSearchIssuesURL(input), { credentials: 'same-origin' })
-    .then(({ issues = [] }) => {
-      const suggestions = issues.map(({ key, fields: { summary } }) => ({
-        content: key, description: escapeEntities(`${key}: ${summary}`)
-      }));
-      suggest(suggestions)
-    });
+  makeJiraApiCall(apiSearchIssuesURL(input), {
+    credentials: 'same-origin'
+  }).then(({ issues = [] }) => {
+    const suggestions = issues.map(({ key, fields: { summary } }) => ({
+      content: key,
+      description: escapeEntities(`${key}: ${summary}`)
+    }));
+    suggest(suggestions);
+  });
 };
 
-const displayTicketSuggestion = (ticket, suggest, subdomain) => {
-  const apiIssueURL = buildHelper(subdomain, apiIssuePath);
-  const key = ticket.toUpperCase();
-
-  showDefaultSuggestion(`Open ${key}`);
-  makeJiraApiCall(apiIssueURL(key)).then(({ fields, key: foundKey, errorMessages = [] } = {}) => {
+const displayProjectSuggestion = (key, suggest, subdomain) => {
+  const apiProjectURL = buildHelper(subdomain, apiProjectPath);
+  showDefaultSuggestion(`Open project ${key}`);
+  makeJiraApiCall(apiProjectURL(key)).then(({ name, key: foundKey, description, errorMessages = [] }) => {
     if (errorMessages.length) {
-      showDefaultSuggestion(`Open ${key}. Heads up, no issue found with this key :(`);
+      showDefaultSuggestion(`Open project ${key}. Heads up, no project found with this key :(`);
     } else {
-      const { summary, status: { name } } = fields;
-      suggest([{ content: foundKey, description: escapeEntities(`${foundKey}: [${name}] ${summary}`) }]);
+      suggest([{
+        content: foundKey,
+        description: escapeEntities([`Project ${name} (${foundKey})`, description].filter(Boolean).join(": "))
+      }]);
     }
   });
 };
 
-const onInputChangedHandler = debounce((input, suggest) => {
+const displayTicketSuggestion = (key, suggest, subdomain) => {
+  const apiIssueURL = buildHelper(subdomain, apiIssuePath);
+  showDefaultSuggestion(`Open issue ${key}`);
+  makeJiraApiCall(apiIssueURL(key)).then(
+    ({ fields, key: foundKey, errorMessages = [] } = {}) => {
+      if (errorMessages.length) {
+        showDefaultSuggestion(
+          `Open issue ${key}. Heads up, no issue found with this key :(`
+        );
+      } else {
+        const {
+          summary,
+          status: { name }
+        } = fields;
+
+        suggest([
+          {
+            content: foundKey,
+            description: escapeEntities(`${foundKey}: [${name}] ${summary}`)
+          }
+        ]);
+      }
+    }
+  );
+};
+
+const onInputChangedHandler = debounce((rawInput, suggest) => {
   get(['jiraSubdomain']).then(({ jiraSubdomain }) => {
+    const input = (rawInput || '').trim().toUpperCase();
+
     if (!jiraSubdomain) {
       showDefaultSuggestion(messages.noSubdomain);
       return;
     }
-    const ticket = ticketMatch(input);
 
-    if (ticket) {
-      displayTicketSuggestion(ticket, suggest, jiraSubdomain);
-      return;
+    const { type, text } = ticketMatch(input);
+    switch (type) {
+      case 'issue': {
+        return displayTicketSuggestion(text, suggest, jiraSubdomain);
+      }
+
+      case 'project': {
+        return displayProjectSuggestion(text, suggest, jiraSubdomain);
+      }
     }
-
-    if (input.trim().length > 2) {
+    if (input.length > 2) {
       displayRelevantSuggestions(input, suggest, jiraSubdomain);
     } else {
       showDefaultSuggestion(messages.typeMore);
@@ -85,17 +124,29 @@ chrome.omnibox.onInputEntered.addListener((input, disposition) => {
     }
 
     if (input && !!input.trim() && jiraSubdomain) {
-      const ticket = ticketMatch(input);
+      const key = input.trim().toUpperCase();
+      const { type, text } = ticketMatch(key);
 
       let url;
-      if (ticket) {
-        const key = ticket.toUpperCase();
-        url = `https://${jiraSubdomain}.atlassian.net/browse/${key}`;
-      } else {
-        const pageSearchIssuesURL = buildHelper(jiraSubdomain, pageSearchIssuesPath)
-        url = pageSearchIssuesURL(input);
-      }
+      switch (type) {
+        case 'issue': {
+          url = `https://${jiraSubdomain}.atlassian.net/browse/${text}`;
+          break;
+        }
 
+        case 'project': {
+          url = `https://${jiraSubdomain}.atlassian.net/projects/${text}`;
+          break;
+        }
+
+        default: {
+          const pageSearchIssuesURL = buildHelper(
+            jiraSubdomain,
+            pageSearchIssuesPath
+          );
+          url = pageSearchIssuesURL(input);
+        }
+      }
       chrome.tabs.update({ url });
     }
   });
